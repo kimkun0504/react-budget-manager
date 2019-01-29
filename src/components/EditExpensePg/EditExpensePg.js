d0ad294ee0570859a0f9f09f2b79f8e0e03ebff6
Kun Kim <kimkun0504@Kuns-MacBook-Air-2.local>
2019-01-29 13:54:49 -0500

    Setup Start Remove Expense Firebase

--- src/components/EditExpensePg/EditExpensePg.js
+++ src/components/EditExpensePg/EditExpensePg.js
@@ -1,30 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import ExpenseForm from '../ExpenseForm/ExpenseForm';
-import { editExpense, removeExpense } from '../../store/actions/expenses';
+import { editExpense, startRemoveExpense } from '../../store/actions/expenses';
 
-const EditExpensePg = (props) => {
-  return (
-    <div>
-      <ExpenseForm
-        expense={props.expense} 
-        onSubmit={(expense) => {
-          props.dispatch(editExpense(props.expense.id, expense));
-          props.history.push('/');
-          console.log('updated', expense);
-        }}
-      />
-      <button onClick={() => {
-            props.dispatch(removeExpense({ id: props.expense.id }));
-            props.history.push('/');
-        }}>Remove</button>
-    </div>
-  );
-  }
+class EditExpensePg extends Component {
+  onSubmit = (expense) => {
+    this.props.editExpense(this.props.expense.id, expense);
+    this.props.history.push('/');
+  };
+  
+  onRemove = () => {
+    this.props.startRemoveExpense({ id: this.props.expense.id });
+    this.props.history.push('/');
+  };
+
+  render() {
+      return (
+        <div>
+          <ExpenseForm
+            expense={this.props.expense} 
+            onSubmit={this.onSubmit}
+          />
+          <button onClick={this.onRemove}>Remove</button>
+        </div>
+      );
+    }
+  };
+const mapDispatchToProps = (dispatch, props) => ({
+  editExpense: (id, expense) => dispatch(editExpense(id, expense)),
+  startRemoveExpense: (data) => dispatch(startRemoveExpense(data)),
+});
+
 const mapStateToProps = (state, props) => {
   return {
     expense: state.expenses.find((expense) => expense.id === props.match.params.id)
   };
 };
 
-export default connect(mapStateToProps)(EditExpensePg);
+export default connect(mapStateToProps, mapDispatchToProps)(EditExpensePg);
